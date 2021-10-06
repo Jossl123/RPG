abf202c3b048e738b2b93d10b3a99f8c13ce5fd9
Jossl123 <jossl1.gau@gmail.com>
2021-10-06 16:11:22 +0200

    pathFindingWork

--- js/pathFinder.js
+++ js/pathFinder.js
@@ -1,18 +1,23 @@
 var notWall = [1]
 
+function arraysEqual(a1, a2) {
+    /* WARNING: arrays must not contain {objects} or behavior may be undefined */
+    return JSON.stringify(a1) == JSON.stringify(a2);
+}
+
 function findAdjacentCell(grid, pos, notWall) {
     var finalList = []
-    if (pos[1] + 1 < grid.lenght && notWall.includes(grid[pos[1] + 1][pos[0]])) { //y + 1
-        finalList.push([pos[1] + 1, pos[0]])
+    if (pos[1] + 1 < grid.length && notWall.includes(grid[pos[1] + 1][pos[0]])) { //y + 1
+        finalList.push([pos[0], pos[1] + 1])
     }
     if (pos[1] - 1 >= 0 && notWall.includes(grid[pos[1] - 1][pos[0]])) { //y - 1
-        finalList.push([pos[1] - 1, pos[0]])
+        finalList.push([pos[0], pos[1] - 1])
     }
-    if (pos[0] + 1 < grid[pos[1]].lenght && notWall.includes(grid[pos[1]][pos[0] + 1])) { //x + 1
-        finalList.push([pos[1], pos[0] + 1])
+    if (pos[0] + 1 < grid[pos[1]].length && notWall.includes(grid[pos[1]][pos[0] + 1])) { //x + 1
+        finalList.push([pos[0] + 1, pos[1]])
     }
     if (pos[0] - 1 >= 0 && notWall.includes(grid[pos[1]][pos[0] - 1])) { //x - 1
-        finalList.push([pos[1], pos[0] - 1])
+        finalList.push([pos[0] - 1, pos[1]])
     }
     return finalList
 }
@@ -21,10 +26,39 @@ function pathFind(grid, startPos, finalPos, notWall) {
     var queue = [
         [finalPos, 0]
     ]
-    for (let pos of queue) {
+    var posInQueue = [finalPos]
+    console.table(grid)
+    for (let i = 0; i < queue.length; i++) {
+        var pos = queue[i]
         var adjacentCell = findAdjacentCell(grid, pos[0], notWall)
-        console.log(adjacentCell)
+        adjacentCell.forEach(cell => {
+            var newPos = [cell, pos[1] + 1]
+            if (!posInQueue.some(a => cell.every((v, i) => v === a[i]))) {
+                queue.push(newPos)
+                posInQueue.push(cell)
+            }
+        });
     }
+    var finalPath = [startPos]
+    for (let i = 0; i < finalPath.length; i++) {
+        var pos = finalPath[i];
+        var adjacentCell = findAdjacentCell(grid, pos, notWall)
+        var min = queue.length
+        var minPos = []
+        adjacentCell.forEach(finalCell => {
+            queue.forEach(queueCell => {
+                if (arraysEqual(queueCell[0], finalCell)) {
+                    if (queueCell[1] < min) {
+                        min = queueCell[1]
+                        minPos = queueCell[0]
+                    }
+                }
+            });
+        });
+        if (arraysEqual(minPos, finalPos)) i = finalPath.length
+        finalPath.push(minPos)
+    }
+    console.log(finalPath)
 }
 
 pathFind([
