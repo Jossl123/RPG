22c7daefbd949b1ceca0ab46cb96b5d6042c2636
Jossl123 <jossl.gau@gmail.com>
2021-10-07 13:21:48 +0200

    continue isometric view and just do it !

--- js/draw.js
+++ js/draw.js
@@ -1,32 +1,56 @@
 function drawRoom(room, player) {
+    // document.getElementById("gameIsoView").innerHTML = ''
+    // for (let y = 0; y < room.length; y++) {
+    //     document.getElementById("gameIsoView").innerHTML += `
+    //     <div class="roomRow" id="roomRow_${y}"></div>`
+    //     for (let x = 0; x < room[y].length; x++) {
+    //         if (room[y][x] == 0) {
+    //             document.getElementById(`roomRow_${y}`).innerHTML += `
+    //             <div class="cell wall" id="cell_${y}_${x}"></div>`
+    //         } else {
+    //             document.getElementById(`roomRow_${y}`).innerHTML += `
+    //             <div class="cell emptyCell" id="cell_${y}_${x}"></div>`
+    //         }
+    //     }
+    // }
+    // document.getElementById("gameIsoClickable").innerHTML = ''
+    // for (let y = 0; y < room.length; y++) {
+    //     document.getElementById("gameIsoClickable").innerHTML += `
+    //     <div class="roomRow" id="roomRowC_${y}"></div>`
+    //     for (let x = 0; x < room[y].length; x++) {
+    //         if (room[y][x] == 0) {
+    //             document.getElementById(`roomRowC_${y}`).innerHTML += `
+    //             <div class="cell" id="cell_${y}_${x}"></div>`
+    //         } else {
+    //             document.getElementById(`roomRowC_${y}`).innerHTML += `
+    //             <div class="cell emptyCellC" id="cell_${y}_${x}"></div>`
+    //         }
+    //     }
+    // }
+
+
     document.getElementById("gameIsoView").innerHTML = ''
-    for (let y = 0; y < room.length; y++) {
-        document.getElementById("gameIsoView").innerHTML += `
-        <div class="roomRow" id="roomRow_${y}"></div>`
-        for (let x = 0; x < room[y].length; x++) {
-            if (room[y][x] == 0) {
-                document.getElementById(`roomRow_${y}`).innerHTML += `
-                <div class="cell wall" id="cell_${y}_${x}"></div>`
-            } else {
-                document.getElementById(`roomRow_${y}`).innerHTML += `
-                <div class="cell emptyCell" id="cell_${y}_${x}"></div>`
+
+    for (let i = room.length - 1; i > 0; i--) {
+        document.getElementById("gameIsoView").innerHTML += `<div style="--rowNbx: ${room.length-i}; --rowNby: ${room.length-i};" id="isoRow${room.length-i}" class="row">`
+        for (let j = 0, x = i; x <= room.length - 1; j++, x++) {
+            console.log(j, (room.length - 1) - x, i)
+            if (room[j][(room.length - 1) - x] >= 1) {
+                document.getElementById(`isoRow${room.length-i}`).innerHTML += `<img class="grassIso" src="img/isometricGrassExample.png" alt="">`
+            }
+        }
+        console.log("----")
+    }
+    for (let i = 0; i <= room.length - 1; i++) {
+        document.getElementById("gameIsoView").innerHTML += `<div style="--rowNbx: ${room.length-i}; --rowNby: ${room.length+i};" id="isoRow${room.length+i}" class="row">`
+        for (let j = 0, y = i; y <= room.length - 1; j++, y++) {
+            if (room[y][j] >= 1) {
+                document.getElementById(`isoRow${room.length+i}`).innerHTML += `<img class="grassIso" src="img/isometricGrassExample.png" alt="">`
             }
         }
     }
-    document.getElementById("gameIsoClickable").innerHTML = ''
-    for (let y = 0; y < room.length; y++) {
-        document.getElementById("gameIsoClickable").innerHTML += `
-        <div class="roomRow" id="roomRowC_${y}"></div>`
-        for (let x = 0; x < room[y].length; x++) {
-            if (room[y][x] == 0) {
-                document.getElementById(`roomRowC_${y}`).innerHTML += `
-                <div class="cell" id="cell_${y}_${x}"></div>`
-            } else {
-                document.getElementById(`roomRowC_${y}`).innerHTML += `
-                <div class="cell emptyCellC" id="cell_${y}_${x}"></div>`
-            }
-        }
-    }
+
+
     drawMiniMap(player)
     drawPlayer(player)
 }
@@ -59,6 +83,4 @@ function drawPlayer(player) {
     //<img src="img/dofusPersoExample.png" class="player" id="player"></img>`
     //document.getElementById(`gameZone`).innerHTML += `
     //<img src="img/dofusPersoExample.png" class="player" id="player"></img>`
-    document.getElementById("player").style.top = `calc(50% + ${32*(player.posOnRoom[1]) - 190})`;
-    document.getElementById("player").style.left = `calc(50% + ${32*(player.posOnRoom[0]) - 190})`;
 }
--- js/worlds.js
+++ js/worlds.js
@@ -1,5 +1,6 @@
-var r = new Array(10).fill(1).map(() => new Array(10).fill(1));
-
+var r = new Array(5).fill(1).map(() => new Array(5).fill(1));
+r[0][0] = 0
+r[1][0] = 0
 var firstWorld = new World("firstWorld", [
     [r, r, 0, r],
     [0, r, r, r],
