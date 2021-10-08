9059cfa248f0edcdf130f4a6e588ed4816699b9b
Jossl123 <71134166+Jossl123@users.noreply.github.com>
2021-10-08 09:38:09 +0200

    movement Work !!!

--- js/movePlayer.js
+++ js/movePlayer.js
@@ -1,18 +1,31 @@
 async function movePlayerTo(x, y) {
+    // pathToGo = pathFind(player.room, player.posOnRoom, [x, y], [1])
+    // var tmpPos = player.posOnRoom
+    // for (let i = 0; i < pathToGo.length; i++) {
+    //     var moveX = pathToGo[i][0] - tmpPos[0]
+    //     var moveY = pathToGo[i][1] - tmpPos[1]
+    //     tmpPos = pathToGo[i]
+    //     movePlayer(moveX, moveY)
+    // }
+
+    var delay = 80,
+        i = 0;
     pathToGo = pathFind(player.room, player.posOnRoom, [x, y], [1])
-    var tmp = player.posOnRoom
-    var moveX = pos[0] - tmp[0]
-    var moveY = pos[1] - tmp[1]
-        // for (let i = 0; i < pathToGo.length; i++) {
-        //     var pos = pathToGo[i]
-        //     var moveX = pos[0] - tmp[0]
-        //     var moveY = pos[1] - tmp[1]
-        //     movePlayer(moveX, moveY)
-        //     tmp = pos
-        // }
+    var tmpPos = player.posOnRoom
+
+    function timeoutLoop() {
+        var moveX = pathToGo[i][0] - tmpPos[0]
+        var moveY = pathToGo[i][1] - tmpPos[1]
+        tmpPos = pathToGo[i]
+        movePlayer(moveX, moveY)
+        if (++i < pathToGo.length)
+            setTimeout(timeoutLoop, delay);
+    }
+
+    setTimeout(timeoutLoop, delay);
 }
 
-function movePlayer(x, y, path) {
+function movePlayer(x, y) {
     if (x == 1) {
         player.right()
     } else if (x == -1) {
@@ -22,12 +35,6 @@ function movePlayer(x, y, path) {
     } else if (y == -1) {
         player.up()
     }
-    var pos = [x, y]
-    var x = path[0][0] - pos[0]
-    var y = path[0][1] - pos[1]
-    drawPlayer(player)
-    path.shift()
-    movePlayer(x, y, path)
 }
 
 function sleep(milliseconds) {
